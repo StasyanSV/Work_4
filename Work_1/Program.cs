3c68f0e12d886b7ee0e14a9cc8bd6f1cf1ff9e44
StasyanSV <stanislav_xx@mail.ru>
2022-10-12 14:45:07 +0400

    add Work_1

---
+++ Work_1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите цикл, который принимает на вход два числа 
+// (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+
+Console.Write("Введите целое число A: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите целое число B: ");
+int B = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Число {A} в натуральной степени числа {B} равно {Gen(A,B)}");
+
+
+int Gen(int A, int B)
+{
+    int result = 1;
+    for (int i = 0; i < B; i++)
+    {
+        result *= A;
+    }
+
+    return result;
+}
