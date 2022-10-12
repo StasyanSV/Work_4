77326f3bb1f650aa1da5187825639e302d03176a
StasyanSV <stanislav_xx@mail.ru>
2022-10-12 15:27:09 +0400

    add Work_3

---
+++ Work_3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19, 2, 4, 9 -> [1, 2, 5, 7, 19, 2, 4, 9 ]
+// 6, 1, 33, 2, 93, 15, 424, 0-> [6, 1, 33, 2, 93, 15, 424, 0]
+
+Console.Clear();
+
+int[] array = new int[8];
+GenMass(array);
+PrintMass(array);
+
+void GenMass(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+        // Console.Write($"{array[i]} ");
+    }
+}
+
+void PrintMass(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
